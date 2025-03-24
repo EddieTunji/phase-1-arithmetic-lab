772be9f7f6722b6309a942ec7c852dd088d59a5f
EddieTunji <davidlundi24@gmail.com>
2025-03-24 08:10:56 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+const num1 = 15.5;
+const num2 = 4;
+const multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random()*100 + 1);
+random;
+
+const num3 = 24;
+const num4 = 10;
+const mod = num3 % num4;
+mod;
+
+const set = (2, 6, 14, 15, 4, 20);
+const max = Math.max(set);
+max;
